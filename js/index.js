d1e52beb53c9a36e02b9d5d76a7f4b481185cf63
ElenaArmand <125710007+ElenaArmand@users.noreply.github.com>
2023-08-22 00:30:53 -0400

    coded work sections, coded UI elements, need to fix nav response, made work cards responsive

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $("#arrow").click(function(){
+   
+                $('html, body').animate({
+                    scrollTop: $("#workSection").offset().top
+                }, 1000);
+       
+})});
+
+$(document).ready(function(){
+   
+    $(".caseStudyContainer").hover(
+        function(){
+       $(this).find(".circlePhotoFrame").css("transform","scale(1.25)");
+       $(this).find(".orangeLine").css("border-top","1px solid #FFCA5F").css("border-bottom","1px solid #FFCA5F"); 
+       $(this).css("border","2px solid #FFCA5F"); 
+    },
+    function(){
+        $(this).find(".circlePhotoFrame").css("transform","scale(1)");
+        $(this).find(".orangeLine").css("border-top","1px solid #E79C48").css("border-bottom","1px solid #E79C48"); 
+        $(this).css("border","none"); 
+    }
+    );
+});
+
+
+// While hovering
+// Change to: "Hover";
+// Animate: Smart animate;
+// Spring: { mass: 1, stiffness: 412, damping: 28.9 };
